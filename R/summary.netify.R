9f1a5cca23581b023a223c03a1aac29efb1cd5b0
s7m <s7.minhas@gmail.com>
2024-05-15 23:08:25 -0400

    add stats and revise documentation for summary fn

--- R/summary.netify.R
+++ R/summary.netify.R
@@ -6,32 +6,63 @@
 #' @param ... Additional parameters which can include user-defined statistical functions. These functions should take a matrix as input and return a scalar value. They will be applied to each network slice individually if the netify object represents longitudinal data.
 #'   - `other_stats`: A named list of functions that take a matrix and return additional actor-level statistics to be included in the output. Each function should accept a matrix as input and return a vector or single value per actor. This allows for the inclusion of custom metrics in the summary output.
 #'
-#' @return A data frame where each row represents the network-level statistics for a single network or a single time point in a longitudinal study. The columns include:
-#' - `numActors`: The number of actors in the network.
-#' - `dens`: The density of the network.
-#' - `recip`: The reciprocity of the network calculated as the correlation between the adjacency matrix and its transpose.
-#' - `sdSendEff`: The standard deviation of the sending effects (row means).
-#' - `sdRecEff`: The standard deviation of the receiving effects (column means).
-#' - `srCov`: The sender-receiver covariance calculated as the correlation between row means and column means.
-#' Additional columns may appear if user-defined functions are passed in the `...` parameter.
-#' Each row is labeled with a network identifier in the `net` column, which is especially useful for longitudinal data.
+#' @return A data frame where each row represents the network-level statistics for a single network or a single time point in a longitudinal study. Depending on the network type and data attributes, the columns can include:
+#' - `num_actors`: Number of actors in the network - for bipartite networks number of row and column actors are reported separately.
+#' - `density`: The proportion of possible connections that are actual connections within the network.
+#' - `num_edges`: The total number of edges in the network.
+#' - `mean_edge_weight`: The average weight of edges in the network, provided only for weighted networks.
+#' - `sd_edge_weight`: The standard deviation of edge weights in the network, provided only for weighted networks.
+#' - `prop_edges_missing`: The proportion of potential edges that are missing or marked as NA.
+#' - `min_edge_value` and `max_edge_value`: The minimum and maximum edge weights observed in the network, provided only for weighted networks.
+#' - `competition_row` and `competition_col`: These statistics measure the competitiveness within the network, quantifying how evenly interactions are spread across actors. High values indicate a decentralized network where interactions are not dominated by a few actors, suggesting a diverse and competitive network structure. This measure is useful across various fields for analyzing the balance of influence or activity among participants in a network. See Dorff, Gallop, & Minhas (2023), "Network Competition and Civilian Targeting during Civil Conflict," British Journal of Political Science, 53(2):441-459 for further details.
+#' - `sd_of_row_means` and `sd_of_col_means`: Standard deviations of the sending and receiving effects (row and column means).
+#' - `covar_of_row_col_means`: The covariance between sending and receiving effects, calculated for unipartite networks.
+#' - `reciprocity`: The reciprocity of the network, defined as the correlation between the adjacency matrix and its transpose, calculated for unipartite networks.
+#' - `transitivity`: The overall transitivity or clustering coefficient of the network, reflecting the likelihood that two neighbors of a node are connected.
 #'
 #' @details This function simplifies the process of extracting key network statistics across potentially multiple networks within a netify object. It is capable of handling both weighted and unweighted networks and can adjust its calculations based on the nature of the network data (cross-sectional vs. longitudinal).
+#' 
+#' @author Cassy Dorff, Shahryar Minhas
 #'
-#' @aliases summary_graph
+#' @examples
+#' data(icews)
+#'
+#' netlet = netify(
+#'     dyad_data=icews, actor1='i', actor2='j',
+#'     time = 'year', symmetric=FALSE, weight='verbCoop' )
+#'
+#' summary(netlet)
+#'
+#' spinglass_ig = function(mat){
+#'     g = prep_for_igraph(mat)
+#'     comm = igraph::cluster_spinglass(g)
+#'     num_comm = length(comm$csize)
+#'     return( c(
+#'         num_comm=num_comm,
+#'         comm_modul = comm$modularity
+#'     ) )
+#' }
+#'
+#' summary(netlet, 
+#'     other_stats=list(spinglass_ig=spinglass_ig)) 
+#' 
+#' @importFrom igraph transitivity
 #' 
 #' @export
 
 summary.netify <- function(object, ...){
 
-  # check if netify object
-  netify_check(object)
+	######################
+	# prelim checks
 
-  # get summary args
-  summary_args <- list(...)
+	# check if netify object
+	netify_check(object)
 
-  # placeholder
-  netlet <- object ; rm(object)
+	# get summary args
+	summary_args <- list(...)
+
+	# placeholder
+	netlet <- object ; rm(object)
 
 	# pull out attrs
 	obj_attrs <- attributes(netlet)
@@ -43,48 +74,112 @@ summary.netify <- function(object, ...){
 	# we can use lapply
 	if(netlet_type == 'cross_sec'){ netlet <- list(netlet) }
 	if(netlet_type == 'longit_array'){ netlet <- array_to_list(netlet) }
+	######################
 
-	# calc some basic stats
-	net_stats <- lapply(netlet, function(mat){
+	######################
+	# calc stats across netlet(s)
+	net_stats_l <- lapply(netlet, function(mat){
 
-		# prelim calcs
-		rowMu <- rowMeans(mat, na.rm=TRUE)
-		colMu <- colMeans(mat, na.rm=TRUE)
+		######################
+		# prep some object
 
-    # vectorized matrix
-    vec_mat <- c(mat)
+		# get igraph version
+		g = prep_for_igraph(mat)
 
-		# measures of interest
-    numActors <- nrow(mat)
-    minVal <- min(vec_mat, na.rm=TRUE)
-    maxVal <- max(vec_mat, na.rm=TRUE)
-	dens <- mean(mat, na.rm=TRUE)
-	recip <- cor(vec_mat, c(t(mat)), use='pairwise.complete.obs')
-	sdSendEff <- sd( rowMu, na.rm=TRUE )
-	sdRecEff <- sd( colMu, na.rm=TRUE )
-	srCov <- cor(c(rowMu), c(colMu), use='pairwise.complete.obs')
+		# bin_mat
+		bin_mat = +(mat > 0)
 
-    # organize
+		# vectorized matrix
+		vec_mat <- c(mat)
+		vec_bin_mat <- c(bin_mat)
+		######################
+
+		######################
+		# start calcs
+
+		# actor variability
+		row_means <- rowMeans(mat, na.rm=TRUE)
+		col_means <- colMeans(mat, na.rm=TRUE)
+
+		# number of actors
+		num_row_actors <- nrow(mat)
+		num_col_actors <- ncol(mat)
+
+		# get proportion missing
+		if(obj_attrs$diag_to_NA){
+			prop_edges_missing = (sum(is.na(vec_mat))+num_row_actors)/length(vec_mat) }
+		if(!obj_attrs$diag_to_NA){
+			prop_edges_missing = sum(is.na(vec_mat))/length(vec_mat) }
+
+		# min/max scores
+		min_edge_value <- min(vec_mat, na.rm=TRUE)
+		max_edge_value <- max(vec_mat, na.rm=TRUE)
+
+		# graph level summary measures
+		density <- mean(vec_bin_mat, na.rm=TRUE)
+		mean_edge_weight <- mean(vec_mat, na.rm=TRUE)
+		sd_edge_weight <- sd(vec_mat, na.rm=TRUE)
+		num_edges <- sum(vec_bin_mat, na.rm=TRUE)
+
+		# competition measures
+		competition_row <- 1-sum(
+			(rowSums(mat, na.rm=TRUE)/sum(vec_mat, na.rm=TRUE))^2)
+		competition_col <- 1-sum(
+			(colSums(mat, na.rm=TRUE)/sum(vec_mat, na.rm=TRUE))^2)
+		
+		# getting at actor variability
+		sd_of_row_means <- sd( row_means, na.rm=TRUE )
+		sd_of_col_means <- sd( col_means, na.rm=TRUE )
+
+		# covar and recip calcs for one mode nets
+		if(obj_attrs$mode=='unipartite'){
+			covar_of_row_col_means <- cor(row_means, col_means, use='pairwise.complete.obs')
+			reciprocity <- cor(vec_mat, c(t(mat)), use='pairwise.complete.obs') }
+
+		# place holder val for bipartite
+		if(obj_attrs$mode=='bipartite'){
+			covar_of_row_col_means <- NA ; reciprocity <- NA }
+
+		# get igraph clustering coef
+		transitivity = igraph::transitivity(g)
+		######################
+
+		######################
+		# organize
 		out <- c(
-      numActors = numActors,
-			dens=dens, recip=recip,
-			sdSendEff=sdSendEff, sdRecEff=sdRecEff, srCov=srCov )
+			num_row_actors = num_row_actors,
+			num_col_actors = num_col_actors,
+			density=density,
+			num_edges = num_edges,
+			mean_edge_weight=mean_edge_weight,
+			sd_edge_weight=sd_edge_weight,
+			prop_edges_missing = prop_edges_missing,			
+			min_edge_value = min_edge_value, 
+			max_edge_value = max_edge_value, 
+			competition_row=competition_row,
+			competition_col=competition_col,
+			sd_of_row_means=sd_of_row_means, 
+			sd_of_col_means=sd_of_col_means,
+			covar_of_row_col_means=covar_of_row_col_means,
+			reciprocity=reciprocity,
+			transitivity=transitivity
+			)
+		######################
 
-    # calculate any userstats
-    # if( 'other_stats' %in% names(summary_args) ){
-    #   other_out <- lapply(summary_args$'other_stats', function(stat){ stat(mat) })
-    #   out <- append(out, unlist(other_out))
-    # }
-    #
-    #
-		  other_out <- lapply(summary_args, function(stat){ stat(mat) })
-		  out <- append(out, unlist(other_out))
-
-    #
+		######################
+		# calculate any user supplied stats
+		if( 'other_stats' %in% names(summary_args) ){
+		  other_out <- lapply(summary_args$'other_stats', function(stat){ stat(mat) })
+		  out <- append(out, unlist(other_out)) }
+		######################
+		
+		#
 		return(out) })
+	######################
 
-  # bind into one matrix
-	net_stats <- do.call('rbind', net_stats)
+	######################
+	# bind into one matrix
+	net_stats <- do.call('rbind', net_stats_l)
 	net_stats <- data.frame(net_stats, stringsAsFactors=FALSE)
 
 	# move variable label to be a column
@@ -93,45 +188,54 @@ summary.netify <- function(object, ...){
 
 	# reorder cols
 	net_stats <- net_stats[,c('net', setdiff(names(net_stats), 'net'))]
+	######################
+
+	######################
+	# clean up default stats based on netlet type
+
+	# if unipartite then collapse num_row_actors and num_col_actors
+	# into one column: num_actors
+	if(obj_attrs$mode=='unipartite'){
+
+		# find num_row_actors
+		n_r_i = which(names(net_stats) == 'num_row_actors')
+		names(net_stats)[n_r_i] = 'num_actors'
+
+		# drop num_col_actors
+		net_stats <- net_stats[,setdiff(names(net_stats), 'num_col_actors')]
+	}
+
+	# if undirected then collapse row and col stats into one
+	if(obj_attrs$symmetric){
+
+		# find competition_row
+		c_r_i = which(names(net_stats) == 'competition_row')
+		names(net_stats)[c_r_i] = 'competition'
+
+		# find sd_of_row_means
+		sd_r_i = which(names(net_stats) == 'sd_of_row_means')
+		names(net_stats)[sd_r_i] = 'sd_of_actor_means'
+
+		# drop covar_of_row_col_means and reciprocity
+		to_drop = c(
+			'competition_col', 'sd_of_col_means',
+			'covar_of_row_col_means', 'reciprocity'
+		)
+		net_stats <- net_stats[,setdiff(names(net_stats), c('covar_of_row_col_means', 'reciprocity'))]
+	}
+
+	# if not weighted then drop redundant stats
+	if(obj_attrs$weight_binary){
+		# drop mean_edge_weight, sd_edge_weight,
+		# min_edge_value, max_edge_value
+		to_drop = c(
+			'mean_edge_weight', 'sd_edge_weight', 
+			'min_edge_value', 'max_edge_value')
+		net_stats <- net_stats[,setdiff(names(net_stats), to_drop)]
+	}
+	######################
 
 	#
 	return(net_stats)
 }
-
-# numActors=numActors,
-# dens=dens
-# recip=recip
-# sdSendEff=sdSendEff
-# sdRecEff=sdRecEff
-# srCov=srCov
-
-# always: 
-	# num_actors: number of actors
-	# num_ties: number of ties
-	# missing: proportion of missing edges	
-	# density: proportion of non-zero ties
-	# competition: tie dispersion. 
-		# To do this, we estimate the relational, or network, competitiveness of the environment. If violence is concentrated around a single actor, where one actor dominates conflict initiation towards many others or receives conflict from many challengers, then civilian victimization will be less likely. If, however, all actors are likely to fight one another -- in an all against all competition -- civilian victimization will be at its highest. To investigate this, we conceptualize the overall strategic environment as a social network, wherein the nodes in this network are armed groups, and the edges are battles between these groups. We formulate our concept of network competition across each conflict network as follows:\footnote{This is a rescaled version of a commonly used measure for market share, the Herfindahl-Hirschman Index.}
-
-		# \begin{equation}
-		# \text{Network Competition} = 1 - \sum_{i = 1}{N} (CS_{i})^2
-		# \end{equation}
-
-		# $N$ denotes the number of actors and $CS_i$ is the conflict share of actor $i$, which is a measure of the proportion of battles an armed actor is involved in.\footnote{This type of measurement of competition has been used in a range of works such as in measuring ethnic and cultural fractionalization \citep{fearon:2003} to the competitiveness of party systems \citep{alfano:baraldi:2015}.} Our measure provides us with a representation of how dispersed conflict is in the network. Figure~\ref{fig:styleNet} provides a conceptual illustration of a low and high competition scenario.
-
-		# The left-hand network exhibits low network competition. Here, conflict patterns are dominated by a single sender or receiver in the network. In this network, an armed group's strategic decision to victimize civilians is fairly straightforward -- victimization takes place if the coercive effect (causing more non-supporters to reluctantly support the group in charge) outweighs the resources that could be mobilized from non-supporters. In this environment, while there may initially be low levels of victimization, we will quickly approach an equilibrium where most civilians support the groups in control of their territory, and no victimization occurs. The right-hand network reveals a different picture. This competitive conflict network functions as a Hobbesian war of all against all, where each armed group is ready to attack each other armed group. Almost all actors are at risk of an attack, and they are at risk of an attack from multiple sources leading to even stronger incentives towards victimization. In this case, there is likely a fluid control of territory. Frequent changes in the system increase incentives for violence against civilians; an assumption supported by existing research \citep{wood:kathman:2015}. Some of the most intractable and dynamic conflicts--like the modern wars in Somalia-- are likely to exhibit this network structure. Our theoretical model, explained below, builds on existing work by formalizing the competitive process in a network environment and including the strategic behavior of civilians as a key predictor of violence against civilians during war.\footnote{\citep{konig:etal:2017} similarly uses a detailed theoretical model to study the interplay between network dynamics and conflict in civil conflict. The main difference is that their study is interested in conflict between armed groups, rather than how conflict between armed groups changes the incentives for one sided violence. In addition, their study focuses on different aspects of the network (actor centrality rather than the overall competitiveness of the network), and their study is an in-depth examination of conflict in the Congo, rather than a cross-national analysis.}
-
-
-# symmetric: 
-	# sd_in_actor_means: variation in actor means
-
-# non-symmetric: 
-	# sd_in_sender_means: variation in sender means
-	# sd_in_receiver_means: variation in receiver means
-	# sender_receiver_rho: correlation between sender and receiver means	
-	# reciprocity: correlation between sending and receiving effects
-
-# weighted: 
-	# mean_edge_weight: average edge weight
-	# sd_edge_weight: variation in edge weights
 
