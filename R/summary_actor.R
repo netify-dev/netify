23c26f46d36c9a154a9fa9e456ef4410e7d99713
s7m <s7.minhas@gmail.com>
2024-05-18 00:37:33 -0400

    rewrite documentation to be more explicit, add in network share measure

--- R/summary_actor.R
+++ R/summary_actor.R
@@ -9,14 +9,26 @@
 #'        should imply stronger (or more valuable) connections rather than longer distances. Default is TRUE.
 #' @param other_stats A named list of functions that take a matrix and return additional actor-level statistics to be included in the output. Each function should accept a matrix as input and return a vector or single value per actor. This allows for the inclusion of custom metrics in the summary output.
 #' @return A `data.frame` object summarizing actor-level statistics of the network(s). Depending on the structure and attributes of the `netify` object, the output includes:
-#' - **Symmetric Unweighted**: Degree, proportion of ties, closeness, betweenness, and eigenvector centrality.
-#' - **Symmetric Weighted**: Degree, proportion of ties, sum and average of strengths, closeness (weighted and normalized), betweenness (weighted and normalized), and eigenvector centrality.
-#' - **Asymmetric Unweighted**: In-degree, out-degree, total degree, in-propensity of ties, out-propensity of ties, total propensity of ties, in-closeness, out-closeness, total closeness, in-betweenness, out-betweenness, total betweenness, authority scores, and hub scores.
-#' - **Asymmetric Weighted**: Similar metrics as asymmetric unweighted but includes strength sums and averages, and all closeness and betweenness measures are weighted.
-#' Metrics are calculated based on the network type (symmetric or asymmetric) and whether the network is weighted.
+#' - **Symmetric Unweighted**: Various network measures for actor \( i \) include:
+#'   - *Degree*: The count of unique actors that actor \( i \) is directly connected to -- calculated as  \eqn{d_i = \sum_{j=1}^{n} a_{ij}}, where \eqn{a_{ij}} is the adjacency matrix element indicating the presence (1) or absence (0) of a tie between actors \eqn{i} and \eqn{j}.
+#'   - *Proportion of ties*: The percentage of actors in the network with whom actor \( i \) has a direct relationship -- calculated as \eqn{p_i = \frac{d_i}{n-1}}, where \eqn{d_i} is the degree of actor \eqn{i} and \eqn{n} is the total number of actors in the network.
+#'   - *Network share*: The fraction of the network's total connections that include actor \( i \) -- calculated as \eqn{s_i = \frac{d_i}{\sum_{j=1}^{n} d_j}}, where \eqn{d_i} is the degree of actor \eqn{i} and \eqn{\sum_{j=1}^{n} d_j} is the total number of ties in the network.
+#'   - *Closeness* (\( C_i \)): A measure of how close actor \( i \) is to all other actors in the network -- calculated as \( C_i = \frac{1}{\sum_{j} d(i, j)} \) where \( d(i, j) \) is the distance to every other actor \( j \).
+#'   - *Betweenness* (\( B_i \)): A measure of actor \( i \)'s importance in connecting different parts of the network -- calculated as \( B_i = \sum_{s \neq i \neq t} \frac{\sigma_{st}(i)}{\sigma_{st}} \) where \( \sigma_{st} \) is the total number of shortest paths from node \( s \) to node \( t \) and \( \sigma_{st}(i) \) is the number of those paths that pass through \( i \).
+#'   - *Eigenvector centrality* (\( EC_i \)): A measure of actor \( i \)'s influence based on their connections to other highly connected actors in the network, calculated using the principal eigenvector of the network's adjacency matrix.
+#' 
+#' - **Symmetric Weighted**: Includes the same statistics as the symmetric unweighted case, with additional measures accounting for the weight of connections:
+#'   - *Strength sum*: The total weight of the ties connected to actor \( i \) in the network -- calculated as \eqn{s_i^{sum} = \sum_{j=1}^{n} w_{ij}}, where \eqn{w_{ij}} is the weight of the tie between actors \eqn{i} and \eqn{j}.
+#'   - *Strength average*: The average weight of the ties connected to actor \( i \) -- calculated as \eqn{s_i^{avg} = \frac{s_i^{sum}}{d_i}}, where \eqn{s_i^{sum}} is the strength sum of actor \eqn{i} and \eqn{d_i} is the degree of actor \eqn{i}.
+#'   - For *closeness* and *betweenness*, edge weights are typically inverted to treat them as distances, following the convention in `igraph`. This behavior can be modified by setting the `invert_weights_for_igraph` parameter in the `summary_actor` function to FALSE.
+#' - **Asymmetric Unweighted**: Same as the symmetric case but now a statistics for the row and column are calculated separately; when relevant a total statistic is calculated as well.
+#' - **Asymmetric Weighted**: Same as the symmetric case but now a statistics for the row and column are calculated separately; when relevant a total statistic is calculated as well.
+#' 
 #' @details The function automatically adjusts calculations based on network symmetry and weight attributes, offering tailored statistical outputs for comprehensive network analysis.
 #' It supports handling of both cross-sectional and longitudinal network data, ensuring that each actor's metrics are accurately computed over time if applicable.
 #' Examples of additional computations (like authority or hub scores) are provided only for asymmetric networks.
+#' 
+#' 
 #' @author Cassy Dorff, Shahryar Minhas
 #'
 #' @examples
@@ -66,6 +78,11 @@ summary_actor <- function( netlet, invert_weights_for_igraph=TRUE, other_stats=N
   # pull out attrs
   obj_attrs <- attributes(netlet)
 
+  # if bipartite then set symmetric parameter
+  # in obj_attrs to FALSE
+  if(obj_attrs$mode == 'bipartite'){
+    obj_attrs$symmetric = FALSE }
+
   # get type
   netify_type <- obj_attrs$netify_type
 
@@ -96,14 +113,8 @@ summary_actor <- function( netlet, invert_weights_for_igraph=TRUE, other_stats=N
       degree <- rowSums(mat>0, na.rm=TRUE)
       prop_ties <- rowMeans(mat>0, na.rm=TRUE)
 
-      # add check for bipartite, if bipartite
-      # then need to append column stats
-      # to rows for simple mat stats
-      if(obj_attrs$mode == 'bipartite'){
-        degree = c( degree, 
-          colSums(mat>0, na.rm=TRUE) )
-        prop_ties = c( prop_ties, 
-          colMeans(mat>0, na.rm=TRUE) ) }
+      # get network share
+      network_share <- degree/sum(degree, na.rm=TRUE)
 
       # igraph stats
       closeness <- igraph::closeness(g, normalized=TRUE)
@@ -114,6 +125,7 @@ summary_actor <- function( netlet, invert_weights_for_igraph=TRUE, other_stats=N
       out <- data.frame(
         degree=degree,
         prop_ties=prop_ties,
+        network_share=network_share,
         closeness=closeness,
         betweenness=betweenness,
         eigen_vector=eigen_vector ) } # symmetric / bin edge case done
@@ -127,18 +139,8 @@ summary_actor <- function( netlet, invert_weights_for_igraph=TRUE, other_stats=N
       strength_sum <- rowSums(mat, na.rm=TRUE)
       strength_avg <- rowMeans(mat, na.rm=TRUE)
 
-      # add check for bipartite, if bipartite
-      # then need to append column stats
-      # to rows for simple mat stats
-      if(obj_attrs$mode == 'bipartite'){
-        degree = c( degree, 
-          colSums(mat>0, na.rm=TRUE) )
-        prop_ties = c( prop_ties, 
-          colMeans(mat>0, na.rm=TRUE) )
-        strength_sum = c( strength_sum,
-          colSums(mat, na.rm=TRUE) )
-        strength_avg = c( strength_avg,
-          colMeans(mat, na.rm=TRUE) ) }
+      # network share
+      network_share <- strength_sum/sum(strength_sum, na.rm=TRUE)
 
       # igraph stats
       closeness <- igraph::closeness(g, normalized=TRUE, weights = g_wgts)
@@ -151,6 +153,7 @@ summary_actor <- function( netlet, invert_weights_for_igraph=TRUE, other_stats=N
         prop_ties=prop_ties,
         strength_sum=strength_sum,
         strength_avg=strength_avg,
+        network_share=network_share,
         closeness=closeness,
         betweenness=betweenness,
         eigen_vector=eigen_vector ) } # symmetric / weighted case end
@@ -170,13 +173,16 @@ summary_actor <- function( netlet, invert_weights_for_igraph=TRUE, other_stats=N
         prop_ties_total <- sum(degree_total > 0, na.rm=TRUE)/length(degree_total) } else {
         prop_ties_total <- c(prop_ties_in, prop_ties_out) }
 
+      # network share
+      network_share_in <- degree_in/sum(degree_in, na.rm=TRUE)
+      network_share_out <- degree_out/sum(degree_out, na.rm=TRUE)
+      network_share_out <- degree_total/sum(degree_total, na.rm=TRUE)
+
       # igraph stats
       closeness_in <- igraph::closeness(g, mode = 'in', normalized=TRUE)
       closeness_out <- igraph::closeness(g, mode = 'out', normalized=TRUE)
       closeness_all = igraph::closeness(g, mode = 'all', normalized=TRUE)
-      betweenness_in <- igraph::betweenness(g, normalized=TRUE)
-      betweenness_out <- igraph::betweenness(g, normalized=TRUE)
-      betweenness_all = igraph::betweenness(g, normalized=TRUE)
+      betweenness <- igraph::betweenness(g, normalized=TRUE)
       authority_score = igraph::authority_score(g)$vector
       hub_score = igraph::hub_score(g)$vector
 
@@ -188,6 +194,9 @@ summary_actor <- function( netlet, invert_weights_for_igraph=TRUE, other_stats=N
         prop_ties_in=prop_ties_in,
         prop_ties_out=prop_ties_out,
         prop_ties_total=prop_ties_total,
+        network_share_in=network_share_in,
+        network_share_out=network_share_out,
+        network_share_total=network_share_total,
         closeness_in=closeness_in,
         closeness_out=closeness_out,
         closeness_all=closeness_all,
@@ -222,6 +231,11 @@ summary_actor <- function( netlet, invert_weights_for_igraph=TRUE, other_stats=N
         strength_avg_total <- (strength_avg_in + strength_avg_out)/2 } else {
         strength_avg_total <- c(strength_avg_in, strength_avg_out) }
 
+      # network share
+      network_share_in <- strength_sum_in/sum(strength_sum_in, na.rm=TRUE)
+      network_share_out <- strength_sum_out/sum(strength_sum_out, na.rm=TRUE)
+      network_share_total <- strength_sum_total/sum(strength_sum_total, na.rm=TRUE)
+
       # igraph stats
       closeness_in <- igraph::closeness(g, mode = 'in', normalized=TRUE, weights = g_wgts)
       closeness_out <- igraph::closeness(g, mode = 'out', normalized=TRUE, weights = g_wgts)
@@ -246,6 +260,9 @@ summary_actor <- function( netlet, invert_weights_for_igraph=TRUE, other_stats=N
         strength_avg_in=strength_avg_in,
         strength_avg_out=strength_avg_out,
         strength_avg_total=strength_avg_total,
+        network_share_in=network_share_in,
+        network_share_out=network_share_out,
+        network_share_total=network_share_total,
         closeness_in=closeness_in,
         closeness_out=closeness_out,
         closeness_all=closeness_all,
