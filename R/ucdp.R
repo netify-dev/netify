7c77dc1aec53e48555804e3602579225ef1021fd
Shahryar Minhas <s7.minhas@gmail.com>
2025-05-20 22:15:53 -0400

    cleanup vignettes

---
+++ R/ucdp.R
@@ -0,0 +1,38 @@
+#' Event data slice from UCDP on Mexico
+#'
+#' Event data from UCDP for Mexico
+#' 
+#' @name mexico
+#' @docType data
+#' @usage data(mexico)
+#' @keywords datasets
+#'
+#' @references add UCDP reference
+#' (\href{https://ucdp.uu.se/}{ucdp})
+#'
+#' @source \href{https://ucdp.uu.se/}{ucdp}
+#'
+#' @examples
+#' data(mexico)
+#' mexico[1:3,]
+NULL
+
+
+#' Event data slice from UCDP on Myanmar
+#'
+#' Event data from UCDP for Myanmar
+#' 
+#' @name myanmar
+#' @docType data
+#' @usage data(myanmar)
+#' @keywords datasets
+#'
+#' @references add UCDP reference
+#' (\href{https://ucdp.uu.se/}{ucdp})
+#'
+#' @source \href{https://ucdp.uu.se/}{ucdp}
+#'
+#' @examples
+#' data(myanmar)
+#' myanmar[1:3,]
+NULL
